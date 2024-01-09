2a231ac496a9bade7aca3897a99e639c47b23729
sherkhan <sherkhan1167746@gmail.com>
2024-01-09 13:31:29 +0500

    implemented image to grayscale with option tpo select from local files

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import streamlit as st
 from PIL import Image
-
+uploaded_image = st.file_uploader("Upload Image")
 with st.expander("Start Camera"):
     camera_image = st.camera_input("camera")
 
+if uploaded_image:
+    img = Image.open(uploaded_image)
+
+    grayscale_image = img.convert("L")
+
+    st.image(grayscale_image)
 if camera_image:
     img = Image.open(camera_image)
 
